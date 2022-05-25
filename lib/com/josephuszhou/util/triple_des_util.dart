2c23d1970fae569aeeacb7bc8435f67a89339f9f
JosephusZhou <josephuszhou@qq.com>
2022-05-25 15:56:53 +0800

    Add 3des module

---
+++ lib/com/josephuszhou/util/triple_des_util.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:convert/convert.dart';
+import 'package:dart_des/dart_des.dart';
+
+import 'log_util.dart';
+
+String? encrypt(String text, String key, String iv) {
+  try {
+    DES3 des3CBC = DES3(
+        key: key.codeUnits,
+        mode: DESMode.CBC,
+        paddingType: DESPaddingType.PKCS5,
+        iv: iv.codeUnits);
+    var encrypted = hex.encode(des3CBC.encrypt(text.codeUnits));
+    dLog(encrypted);
+    return encrypted;
+  } catch (e) {
+    dLog(e);
+  }
+  return null;
+}
+
+String? decrypt(String text, String key, String iv) {
+  try {
+    DES3 des3CBC = DES3(
+        key: key.codeUnits,
+        mode: DESMode.CBC,
+        paddingType: DESPaddingType.PKCS5,
+        iv: iv.codeUnits);
+    var decrypted = utf8.decode(des3CBC.decrypt(hex.decode(text)));
+    dLog(decrypted);
+    return decrypted;
+  } catch (e) {
+    dLog(e);
+  }
+  return null;
+}
