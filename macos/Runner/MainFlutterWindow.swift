632dc6c91e48456192c870e4984b83741e942881
JosephusZhou <josephuszhou@qq.com>
2022-10-20 11:38:59 +0800

    Renew macos icons and window size

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -7,7 +7,7 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-    self.setContentSize(NSSize(width: 500, height: 800))
+    self.setContentSize(NSSize(width: 920, height: 840))
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
